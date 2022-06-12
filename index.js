3d16703a69c4135ac284f8b30775d171b68cf66c
codeandcodeandcode <codepkcode49@gmail.com>
2022-06-12 16:55:45 +0300

    Created a function to declare a variable in the global scope

--- index.js
+++ index.js
@@ -3,3 +3,8 @@ var customerName = "bob";
 const upperCaseCustomerName = ()=>{
     return customerName.toUpperCase();
 }
+
+const setBestCustomer = ()=>{
+    var bestCustomer = 'not bob';
+    return bestCustomer;
+}
